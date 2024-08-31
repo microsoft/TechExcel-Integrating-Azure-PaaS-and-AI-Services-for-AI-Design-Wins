04b727371cd6b711b9ed04fc7d8b66e4c29bccb7
kylebunting <kyle.bunting@gmail.com>
2024-08-31 16:02:42 -0600

    Adding function app for document vectorization

---
+++ src/ContosoSuitesVectorizationFunction/CosmosChangeFeedVectorization.cs
@@ -0,0 +1,121 @@
+using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using OpenAI.Embeddings;
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace ContosoSuites.Functions
+{
+    public class CosmosChangeFeedVectorization
+    {
+        private readonly ILogger _logger;
+        private readonly EmbeddingClient _embeddingClient;
+        private const string DatabaseName = "ContosoSuites";
+        private const string ContainerName = "MaintenanceTasks";
+
+        public CosmosChangeFeedVectorization(ILoggerFactory loggerFactory)
+        {
+            var endpointUrl = Environment.GetEnvironmentVariable("AzureOpenAIEndpont");
+            if (string.IsNullOrEmpty(endpointUrl))
+                throw new ArgumentNullException("AzureOpenAIEndpont", "AzureOpenAIEndpont is required to run this function.");
+
+            var azureOpenAIKey = Environment.GetEnvironmentVariable("AzureOpenAIKey");
+            if (string.IsNullOrEmpty(azureOpenAIKey))
+                throw new ArgumentNullException("AzureOpenAIKey", "AzureOpenAIKey is required to run this function.");
+
+            var deploymentName = Environment.GetEnvironmentVariable("DeploymentName");
+            if (string.IsNullOrEmpty(deploymentName))
+                throw new ArgumentNullException("DeploymentName", "DeploymentName is required to run this function.");
+
+            _logger = loggerFactory.CreateLogger<CosmosChangeFeedVectorization>();
+            var oaiEndpoint = new Uri(endpointUrl);
+            var credentials = new AzureKeyCredential(azureOpenAIKey);
+            var openAIClient = new AzureOpenAIClient(oaiEndpoint, credentials);
+            _embeddingClient = openAIClient.GetEmbeddingClient(deploymentName);
+            
+        }
+
+        [Function("VectorizeMaintenanceTasks")]
+        [CosmosDBOutput(DatabaseName, ContainerName, Connection = "CosmosDBConnectionString")]
+        public object Run([CosmosDBTrigger(
+            databaseName: DatabaseName,
+            containerName: ContainerName,
+            Connection = "CosmosDBConnectionString",
+            LeaseContainerName = "leases",
+            CreateLeaseContainerIfNotExists = true)] IReadOnlyList<MaintenanceTask> input)
+        {
+            IReadOnlyList<MaintenanceTask> documentsToVectorize = input.Where(t => t.Type != "Vectorized");
+            if (documentsToVectorize.Count() == 0) return null;
+
+            foreach (var task in documentsToVectorize)
+            {
+                try
+                {
+                    // Generate the content vector for the maintenance task.
+                    var embedding = _embeddingClient.GenerateEmbedding(task.Details);
+                    var contentVector = embedding.Value.Vector;
+                    // Add the content vector to the maintenance task and mark it as vectorized.
+                    task.ContentVector = contentVector.ToArray();
+                    task.Type = "Vectorized";
+                    _logger.LogInformation($"Generated content vector for maintenance task {task.Id}");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Error generating content vector for maintenance task {task.Id}");
+                }
+            }
+
+            // Write the updated documents back to Cosmos DB.
+            return documentsToVectorize;
+        }
+    }
+
+    /// <summary>
+    /// Represents a maintenance task.
+    /// </summary>
+    public class MaintenanceTask
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("type")]
+        public string? Type { get; set; }
+        
+        [JsonPropertyName("hotel_id")]
+        public int HotelId {get;set;}
+        
+        [JsonPropertyName("hotel")]
+        public string Hotel { get; set; }
+
+        [JsonPropertyName("source")]
+        public string Source { get; set; }
+
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+
+        [JsonPropertyName("details")]
+        public string Details { get; set; }
+        
+        [JsonPropertyName("room_number")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? RoomNumber { get; set; }
+
+        [JsonPropertyName("room_numbers_checked")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? RoomNumbersChecked { get; set; }
+        
+        [JsonPropertyName("meeting_room")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public int? MeetingRoom { get; set; }
+        
+        [JsonPropertyName("location")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? Location { get; set; }
+
+        [JsonPropertyName("content_vector")]
+        public float[]? ContentVector { get; set; }
+    }
+}
