40263e684811ff16f7a3375c23c1e53769a02463
kylebunting <kyle.bunting@gmail.com>
2024-09-02 16:20:48 -0600

    Adding vector search functionality

--- src/ContosoSuitesWebAPI/Services/IVectorizationService.cs
+++ src/ContosoSuitesWebAPI/Services/IVectorizationService.cs
@@ -1,7 +1,10 @@
-﻿namespace ContosoSuitesWebAPI.Services
+﻿using ContosoSuitesWebAPI.Entities;
+
+namespace ContosoSuitesWebAPI.Services
 {
     public interface IVectorizationService
     {
         Task<ReadOnlyMemory<float>> GetEmbeddings(string text); 
+        Task<List<VectorSearchResult>> ExecuteVectorSearch(float[] queryVector, int count = 5);
     }
 }
--- src/ContosoSuitesWebAPI/Services/VectorizationService.cs
+++ src/ContosoSuitesWebAPI/Services/VectorizationService.cs
@@ -1,10 +1,14 @@
 ﻿using Azure.AI.OpenAI;
+using ContosoSuitesWebAPI.Entities;
+using Microsoft.Azure.Cosmos;
+using System.Text.Json.Serialization;
 
 namespace ContosoSuitesWebAPI.Services
 {
-    public class VectorizationService(AzureOpenAIClient client, IConfiguration configuration) : IVectorizationService
+    public class VectorizationService(AzureOpenAIClient openAIClient, CosmosClient cosmosCient, IConfiguration configuration) : IVectorizationService
     {
-        private readonly AzureOpenAIClient _client = client;
+        private readonly AzureOpenAIClient _client = openAIClient;
+        private readonly CosmosClient _cosmosCient = cosmosCient;
         private readonly string _deploymentName = configuration.GetValue<string>("DEPLOYMENT_NAME") ?? "text-embedding-ada-002";
 
         public async Task<ReadOnlyMemory<float>> GetEmbeddings(string text)
@@ -24,6 +28,29 @@ namespace ContosoSuitesWebAPI.Services
             {
                 throw new Exception($"An error occurred while generating embeddings: {ex.Message}");
             }
+        }
+
+        public async Task<List<VectorSearchResult>> ExecuteVectorSearch(float[] queryVector, int count = 0)
+        {
+            var db = _cosmosCient.GetDatabase(configuration.GetValue<string>("COSMOS_DATABASE_NAME") ?? "ContosoSuites");
+            var container = db.GetContainer(configuration.GetValue<string>("MAINTENANCE_REQUESTS_CONTAINER_NAME") ?? "MaintenanceRequests");
+
+            var query = $"SELECT c.hotel_id AS HotelId, c.hotel AS Hotel, c.details AS Details, c.source AS Source, VectorDistance(c.request_vector, [{string.Join(",", queryVector)}]) AS SimilarityScore FROM c";
+            query += $" ORDER BY VectorDistance(c.request_vector, [{string.Join(",", queryVector)}])";
+
+            var results = new List<VectorSearchResult>();
+            Console.Write($"Executing query/n/n{query}");
+            using var feedIterator = container.GetItemQueryIterator<VectorSearchResult>(new QueryDefinition(query));
+            while (feedIterator.HasMoreResults)
+            {
+                foreach(var item in await feedIterator.ReadNextAsync())
+                {
+                    results.Add(item);
+                }
+            }
+            return count > 0 ? results.Take(count).ToList() : [.. results];
+
+            //throw new NotImplementedException();
         }
     }
 }
