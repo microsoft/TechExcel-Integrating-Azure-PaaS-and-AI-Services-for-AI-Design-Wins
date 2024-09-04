5fad50439aa8c81243173f91c57b941581a09ae1
kylebunting <kyle.bunting@gmail.com>
2024-09-03 19:30:11 -0600

    Fixing method signature in interface

--- src/ContosoSuitesWebAPI/Services/IVectorizationService.cs
+++ src/ContosoSuitesWebAPI/Services/IVectorizationService.cs
@@ -4,7 +4,7 @@ namespace ContosoSuitesWebAPI.Services
 {
     public interface IVectorizationService
     {
-        Task<ReadOnlyMemory<float>> GetEmbeddings(string text); 
+        Task<float[]> GetEmbeddings(string text); 
 
         // Exercise 3 Task 3 TODO #1: Uncomment the following line of code to provide an execute a vector search method against Cosmos DB.
         //Task<List<VectorSearchResult>> ExecuteVectorSearch(float[] queryVector, int count = 0);
