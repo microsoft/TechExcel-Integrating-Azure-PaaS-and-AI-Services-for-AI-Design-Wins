0ba398c0aa5a65de0dad7147a4aa9ce69043f5aa
Monica Dodero <74478059+mlmn2820@users.noreply.github.com>
2024-09-26 22:04:30 +0000

    Initial commit

--- src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
+++ src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
@@ -11,6 +11,10 @@ def create_chat_completion(messages):
     aoai_key = st.secrets["aoai"]["key"]
     aoai_deployment_name = st.secrets["aoai"]["deployment_name"]
 
+    search_endpoint = st.secrets["search"]["endpoint"]
+    search_key = st.secrets["search"]["key"]
+    search_index_name = st.secrets["search"]["index_name"]
+
     client = openai.AzureOpenAI(
         api_key=aoai_key,
         api_version="2024-06-01",
@@ -23,7 +27,22 @@ def create_chat_completion(messages):
             {"role": m["role"], "content": m["content"]}
             for m in messages
         ],
-        stream=True
+        stream=True,
+        extra_body={
+            "data_sources": [
+                {
+                    "type": "azure_search",
+                    "parameters": {
+                        "endpoint": search_endpoint,
+                        "index_name": search_index_name,
+                        "authentication": {
+                            "type": "api_key",
+                            "key": search_key
+                        }
+                    }
+                }
+            ]
+        }
     )
 
 def handle_chat_prompt(prompt):
