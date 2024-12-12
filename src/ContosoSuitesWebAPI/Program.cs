6ef33e6cb7a8d217171fb7c5d6e1ff20a7e596c7
Aditya <apkola29@gmail.com>
2024-12-12 22:52:27 +0000

    test test

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -103,6 +103,32 @@ app.MapGet("/Vectorize", async (string text, [FromServices] IVectorizationServic
 })
     .WithName("Vectorize")
     .WithOpenApi();
+// Retrieve the set of hotels from the database.
+app.MapGet("/Hotels", async () => 
+{
+    var hotels = await app.Services.GetRequiredService<IDatabaseService>().GetHotels();
+    return hotels;
+})
+    .WithName("GetHotels")
+    .WithOpenApi();
+
+// Retrieve the bookings for a specific hotel.
+app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) => 
+{
+    var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsForHotel(hotelId);
+    return bookings;
+})
+    .WithName("GetBookingsForHotel")
+    .WithOpenApi();
+
+// Retrieve the bookings for a specific hotel that are after a specified date.
+app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) => 
+{
+    var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsByHotelAndMinimumDate(hotelId, min_date);
+    return bookings;
+})
+    .WithName("GetRecentBookingsForHotel")
+    .WithOpenApi();
 
 // This endpoint is used to search for maintenance requests based on a vectorized query.
 app.MapPost("/VectorSearch", async ([FromBody] float[] queryVector, [FromServices] IVectorizationService vectorizationService, int max_results = 0, double minimum_similarity_score = 0.8) =>
